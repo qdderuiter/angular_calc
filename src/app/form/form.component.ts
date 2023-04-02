eb58be513187c8cd6e92a56d9f6fb036295398bf
Quinten de Ruiter <119458406+qdderuiter@users.noreply.github.com>
2023-04-02 21:12:52 +0200

    Added variables in form html

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
-
+  berekenen() {
+    
+  }
 }
