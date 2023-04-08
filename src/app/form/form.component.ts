0d8ddd5829ccf51ea2301d4f26c0453e1021bcb9
Quinten de Ruiter <119458406+qdderuiter@users.noreply.github.com>
2023-04-08 11:53:55 +0200

    Added service for localstorage

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from '../local-storage.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent {
+  constructor (private localStore: LocalStorageService) {
+  }
+
   inkomen: number = 0;
   uitgaven_gezamenlijk: number = 0
   uitgaven_persoonlijk: number = 0
@@ -14,8 +18,25 @@ export class FormComponent {
   spaardoelen: number = 0
   resterendMaandBedrag: number = 0;
 
+
+  saveToLocalStorage() {
+    this.localStore.saveData('inkomen', JSON.stringify(this.inkomen))
+    this.localStore.saveData('uitgaven_gezamenlijk', JSON.stringify(this.uitgaven_gezamenlijk))
+    this.localStore.saveData('uitgaven_persoonlijk', JSON.stringify(this.uitgaven_persoonlijk))
+    this.localStore.saveData('aandelen', JSON.stringify(this.aandelen))
+    this.localStore.saveData('crypto', JSON.stringify(this.crypto))
+    this.localStore.saveData('spaardoelen', JSON.stringify(this.spaardoelen))
+    this.localStore.saveData('resterent_maandbedrag', JSON.stringify(this.resterendMaandBedrag))
+  }
   
   berekenen() {
-    this.resterendMaandBedrag = this.inkomen - this.uitgaven_gezamenlijk - this.uitgaven_persoonlijk - this.aandelen - this.crypto - this.spaardoelen
+    this.resterendMaandBedrag = this.inkomen - this.uitgaven_gezamenlijk - this.uitgaven_persoonlijk - this.aandelen - this.crypto - this.spaardoelen;
+    this.saveToLocalStorage();
+    this.getValueFromLocalStorage()
+}
+
+  getValueFromLocalStorage() {
+    this.localStore.getData('inkomen')
   }
+
 }
